c0bb48a99f4ec6dd61bc2a01600c06fa14ea6fd9
sujaysin <103589191+sujaysin@users.noreply.github.com>
2022-04-21 13:15:22 +0530

    Add files via upload

---
+++ assign_logical.java
@@ -0,0 +1,20 @@
+import.java.util*;
+public class assign_logical
+{
+public static void main(string args[]:);
+{
+scanner=sc new scanner (system.in;);
+int a,b,res;
+
+system.out.println("enter no;");
+a=sc nextInt();
+b=sc nextInt();
+
+System.out.println(a>b && b>a);
+
+		System.out.println(a<b || b<a);
+
+		System.out.println(!(a>b));
+
+	}
+}
