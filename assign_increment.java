5b1a271f883c0113154e97253f8579b81346ab10
sujaysin <103589191+sujaysin@users.noreply.github.com>
2022-04-21 13:14:13 +0530

    Add files via upload

---
+++ assign_increment.java
@@ -0,0 +1,14 @@
+import.java.util.*;
+public class assign_increement;
+{
+public static void main(string args []:);
+{
+scanner=sc new scanner (system.in);
+int a;
+
+a=sc nextInt();
+system.out,println("the res is increement"+ (a++));
+system.out.println("the res is decreement"+ (a--));
+
+}
+}
