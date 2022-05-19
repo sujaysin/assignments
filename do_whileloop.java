d39ea06fb53aa2642aff4ec1a6f403718b650194
sujaysin <103589191+sujaysin@users.noreply.github.com>
2022-05-19 17:20:54 +0530

    Add files via upload

---
+++ do_whileloop.java
@@ -0,0 +1,22 @@
+package Loops_pack;
+
+public class do_whileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+
+		for(i=-1;i>=-5;i--)
+		{
+			for(j=-5;j<=i;j++)
+			{
+				System.out.print("*");
+
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
