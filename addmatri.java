64b0ebee30782194e52af9d3a719a1656d67a1b1
sujaysin <103589191+sujaysin@users.noreply.github.com>
2022-05-19 17:20:01 +0530

    Add files via upload

---
+++ addmatri.java
@@ -0,0 +1,46 @@
+
+package arrays;
+
+import java.util.Scanner;
+///Transpose of matrix
+public class addmatri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int i, j;  
+		    System.out.println("Enter total rows and columns: ");  
+		    Scanner s = new Scanner(System.in);  
+		    int row = s.nextInt();  
+		    int column = s.nextInt();  
+		    int array[][] = new int[row][column];  
+		    System.out.println("Enter matrix:");  
+		    for(i = 0; i < row; i++)  
+		    {  
+		        for(j = 0; j < column; j++)   
+		            {  
+		            array[i][j] = s.nextInt();  
+
+		            }  
+		    }  
+		    System.out.println("The above matrix before Transpose is ");  
+		    for(i = 0; i < row; i++)  
+		        {  
+		            for(j = 0; j < column; j++)  
+		            {  
+		            System.out.print(array[i][j]+" ");  
+		            }  
+		            System.out.println(" ");  
+		        }  
+		    System.out.println("The above matrix after Transpose is ");  
+		    for(i = 0; i < column; i++)  
+		        {  
+		            for(j = 0; j < row; j++)  
+		            {  
+		                System.out.print(array[j][i]+" ");  
+		            }  
+		            System.out.println(" ");  
+		        }  
+
+	}
+
+}
